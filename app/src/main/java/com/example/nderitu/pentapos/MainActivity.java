cd357fbce614e344c6df5201f5f45c9c237ef1ba
Nderitu <code.nderitu@gmail.com>
2017-07-10 07:10:10 +0300

    Start of the project

---
+++ app/src/main/java/com/example/nderitu/pentapos/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nderitu.pentapos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView theHelloText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
